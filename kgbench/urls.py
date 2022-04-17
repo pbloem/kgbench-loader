cad0914467b5226638237c1aefee0405225e4c1e
Peter Bloem <git@peterbloem.nl>
2022-04-17 15:23:47 +0200

    Update urls.

--- kgbench/urls.py
+++ kgbench/urls.py
@@ -3,5 +3,11 @@ Dictionary  containing the download URLS for the datasets.
 """
 
 URLS = {
-    'amplus': ['https://www.dropbox.com/s/12yp5exeuujfkyp/amplus.tgz?dl=1']
+    'aifb' : ['https://www.dropbox.com/s/pig4tu771akgll0/aifb.tgz?dl=1'],
+    'amplus': ['https://www.dropbox.com/s/12yp5exeuujfkyp/amplus.tgz?dl=1'],
+    'dblp' : ['https://www.dropbox.com/s/zc3pdho1k7xr520/dblp.tgz?dl=1'],
+    'mdgenre': ['https://www.dropbox.com/s/uksmcbfv23cjsa8/mdgenre.tgz?dl=1'],
+    'mdgender': ['https://www.dropbox.com/s/irb997j1yc6ohq3/mdgender.tgz?dl=1'],
+    'dmgfull': ['https://www.dropbox.com/s/59tfdawqdxvpvm8/dmgfull.tgz?dl=1'],
+    'dmg777k': ['https://www.dropbox.com/s/fal1nobf1etxtfu/dmg777k.tgz?dl=1']
 }
